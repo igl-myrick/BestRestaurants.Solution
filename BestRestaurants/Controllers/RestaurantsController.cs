d8bf2425010442003997e8a6cb9d0645dc9252b4
igl-myrick <igl.myrick@gmail.com>
2024-11-20 14:00:19 -0800

    add Index route

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants
+        .Include(restaurant => restaurant.Cuisine)
+        .ToList();
+      return View(model);
+    }
+  }
+}
